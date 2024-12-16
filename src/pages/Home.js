0606a0f451721cd2a78fdae019cd938c25c9f5a9
ricardoherreral <l19120189@morelia.tecnm.mx>
2024-12-16 16:12:46 -0600

    chore(frontend): frontend finished

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='mb-20'>
+        <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>Hello, welcome to my blog</h1>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Cillum sunt exercitation tempor Lorem et do. Nostrud aliqua aliqua ullamco nisi pariatur minim magna sint minim id Lorem deserunt. Mollit amet ad tempor sit nostrud veniam id fugiat fugiat Lorem voluptate. Ea reprehenderit consequat cupidatat exercitation reprehenderit aliquip consectetur occaecat eiusmod eiusmod ea consectetur Lorem culpa. Labore quis irure reprehenderit sit pariatur amet laborum quis. Proident eiusmod et sit pariatur Lorem aliqua sunt sunt. Eiusmod ad velit esse incididunt pariatur reprehenderit id.</p>
+        <p className='mx-auto leading-relaxed text-base mb-4'>Laboris reprehenderit quis labore eu. Consectetur exercitation duis irure culpa dolore magna sit labore ut elit et magna officia nulla. Ipsum tempor in veniam laborum consectetur aliqua minim excepteur ad. Proident nulla enim commodo quis ex et qui sint aliquip laboris dolor non. Aliqua tempor nulla dolore labore enim ad duis eiusmod ipsum. Aute sunt veniam dolore consequat commodo exercitation aliquip exercitation.</p>
+    </div>
+  )
+}
+
+export default Home
